7270328247f223a9aa00af9acb251eeef9af19a4
Matt Alexander <mattjalexander@gmail.com>
2017-02-06 12:19:29 -0800

    Jumping back to laptop.

--- cop2dynamo.py
+++ cop2dynamo.py
@@ -1,52 +1,63 @@
 import csv
 import sys
 
-# expectec columns for oers
-# 20170131_oers.csv:
-# UPC
-# NAME
-# GRADE
-# LastRatingEnd
-# RatingDue
-# RatingStatus
-# RatingPeriod
-# RatingType
-# Rater
-# IntermediateRater
-# SeniorRater
-# Notes
-source_dir = r'C:\Users\Matt\Documents\SpiderOak Hive\3-161\S1\evals\oers'
+# oer csv mappings  -> SMs DDB
 
-# Table: SMs
-# attributes:
-#   RatingBegin
-#   RatingEnd
-#   Unit
-#   Rater
-#   Sr. Rater
-#   Rank
-#   RatingType
-#   Comments: {'date': ''}
+# 0 UPC                                       -> upc
+# 1 NAME                                      -> name (gsi)
+#                                             -> email
+#                                             -> dodid [primary key]
+# 2 GRADE                                     -> grade
+# 3 LastRatingEnd                             -> last_rating_end
+# 4 RatingDue     // doesn't exist for ncoers -> rating_due
+# 5 RatingStatus                              -> rating_status
+# 6 RatingPeriod                              -> rating_period
+# 7 RatingType                                -> rating_type
+# 8 Rater                                     -> rater
+# 9 IntermediateRater                         -> intermediate_rater
+# 10 SeniorRater                              -> senior_rater
+# 11 Reviewer  // doesn't exist for oers      -> reviewer
+# 12 Notes                                    -> notes{'date': ''}
+# 13 Legacy NCOER Comments // may not exist   -> notes{'date-legacyNCOER': ''}
+
+source = r'C:\Users\Matt\Documents\SpiderOak Hive\3-161\S1\evals\oers\20160828_oers.csv'
 
 # initialize dynamo connnection
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('SMs')
+#dynamodb = boto3.resource('dynamodb')
+#table = dynamodb.Table('SMs')
 
 with open(source, newline='') as csvfile:
+    type = 'unknown'
     for row in csv.reader(csvfile, delimiter=','):
+
         # skip the row if it looks like a header.
         # the less we trust the source the cleverer we have to be here.
-        if row[1] == 'DODID':
+        if row[0] == 'UPC':
+            # id if this is an oer or ncoer
+            if row[11] == 'Reviewer':
+                type = 'ncoer'
+            if row[4] == 'RatingDue'
+                type = 'oer'
+            print("Dealing with a " + type)
             continue
 
-        # apparently some people don't have email. that's ok i guess.
-        # (nb: dynamo isn't happy at empty values)
-        if row[2] == '':
-            row[2] = '.'
+        # i should probably make an SM object. meh.
+        upc = row[0]
+        name = row[1]
+        grade = row[2]
+        last_rating_end = row[3]
+        rating_due = row[4] # doesn't exist for ncoers
+        rating_status = row[5]
+        rating_period = row[6]
+        rating_type = row[7]
+        rater = row[8]
+        intermediate_rater = row[9]
+        senior_rater = row[10]
+        reviewer = row[11]
 
-        # Update dynamo.
-        table.put_item(Item={
-            'name': row[0],
-            'DODID': row[1],
-            'email': row[2]
-            })
+        if reviewer == '':
+            notes = row[12]
+
+        print("upc: ", upc)
+        print("name:", name)
+        print("---------")
